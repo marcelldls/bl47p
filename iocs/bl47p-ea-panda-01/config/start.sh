d4d905c51442009de1e774ce7715f489f4c4891f
Giles Knap <giles.knap@diamond.ac.uk>
2024-01-11 16:05:16 +0000

    fix panda ioc instance

--- iocs/bl47p-ea-panda-01/config/start.sh
+++ iocs/bl47p-ea-panda-01/config/start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-mkdir -p /epics/opi
 
-pandablocks-ioc softioc bl47p-mo-panda-01 BL47P-EA-PANDA-01 --clear-bobfiles --screens-dir /epics/opis
-
-
+pandablocks-ioc softioc 192.168.250.18 BL47P-MO-PANDA-01 --clear-bobfiles --screens-dir /epics/opi
